de71e1cf7b4c71ab2e6e89c2807b51bfbc1ec77b
hunter balling <muvimi0404@gmail.com>
2020-08-02 01:12:52 +0200

    Services Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,36 +2,36 @@
   <div id="about">
     <div class="container">
  
-      <div class="row">
+      <div class="row pt-5">
         <div class="col-md-3">
           <div class="experience">
-          <div class="years">2+</div>
-          <div class="experience-text">Website Development Experience</div>
+          <div class="years text-red">2+</div>
+          <div class="experience-text text-red">Years Of Website Development Experience</div>
           </div>
         </div>
         <div class="col-md-9 services">
-        <div class="about-title">
-          <h1>A Bit About Myself</h1>
-          <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h4>
+        <div class="about-title text-center">
+          <h1 class="border-bottom">A Bit About My Super Powers</h1>
+          <h4><small class="pt-2"><q>Yes We Can!!</q> ~ Barack Obama</small></h4>
         </div>
-          <div class="d-flex">
-            <div class="web-design">
-              <h4>Lorem, ipsum dolor.</h4>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus voluptatum et magnam iusto commodi aspernatur amet. Harum similique delectus beatae temporibus excepturi aut ullam id fugiat tempora cum numquam illo dolorem, dignissimos officiis consequatur optio?</p>
+          <div class="row pt-5">
+            <div class="web-design col">
+              <h4>Website Design</h4>
+              <p>Grow your business with an interacting, engaging website that boosts your online footprint. We will also help you in getting  agoogle listing for you business to ensure your online visibility is maximized. get your website now.</p>
             </div>
-            <div class="graphic-design">
-              <h4>Lorem, ipsum dolor.</h4>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit neque, ab earum magnam modi, nisi expedita a maxime soluta natus optio ex fugit temporibus, assumenda obcaecati odit saepe! Necessitatibus voluptas, a aliquam fugiat totam ullam!</p>
+            <div class="graphic-design col">
+              <h4>Graphic Design.</h4>
+              <p>I work in tandem with my clients to develop marketing campaigns that are engaging and effective. I specialize in <span class="text-red">Motion Graphics, Marketing and Advertising Graphic and User Interface Graphic Design.</span></p>
             </div>
           </div>
-           <div class="d-flex">
-            <div class="Concept Design">
-              <h4>Lorem, ipsum dolor.</h4>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus voluptatum et magnam iusto commodi aspernatur amet. Harum similique delectus beatae temporibus excepturi aut ullam id fugiat tempora cum numquam illo dolorem, dignissimos officiis consequatur optio?</p>
+           <div class="row pt-3">
+            <div class="concept-design col">
+              <h4> Software Concept Design</h4>
+              <p>Are you having trouble kicking off your software design process? Let me guide you step by step in coming up with various concepts to solving you individual problem.</p>
             </div>
-            <div class="Social-markerting">
-              <h4>Lorem, ipsum dolor.</h4>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit neque, ab earum magnam modi, nisi expedita a maxime soluta natus optio ex fugit temporibus, assumenda obcaecati odit saepe! Necessitatibus voluptas, a aliquam fugiat totam ullam!</p>
+            <div class="social-markerting col">
+              <h4>Social Media Marketing</h4>
+              <p>Let us leverage the infinite marketing power that comes along with having your business on social media. Do not wait for your clients to come to you, take you business to them.</p>
             </div>
           </div>
         </div>
@@ -53,7 +53,16 @@ export default {
   background-size: cover;
   width:100vw;
   height:100vh;
-  overflow-wrap: initial;
-  background-color:lightgray;
+  overflow-x:none;
+  background-color:rgb(61, 2, 2);
+}
+.years{
+  font-size: 8em;
+}
+.experience-text{
+  font-size: 1.8em;
+}
+.text-red{
+  color:rgb(87, 11, 11);
 }
 </style>
